cb09b50b19c8a45be311d0a6124c9adf3849f020
E.vinothkumar <90100624+vino160898@users.noreply.github.com>
2021-10-14 18:19:10 +0530

    Update Bubble_sort.py

--- Bubble_sort.py
+++ Bubble_sort.py
@@ -4,5 +4,5 @@ for i in range(1,len(l)):
 	for i in range(1,len(l)-i):
 		if l[i]>l[i+1]:
 			l[i],l[i+1]=l[i+1],l[i]
-else:
-	print(l)  #-->[23, 20, 28, 34, 40, 45, 78]
+	else:
+		print(l)  #-->[23, 20, 28, 34, 40, 45, 78]
